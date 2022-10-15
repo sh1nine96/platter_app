ec0fe111dcabf707f191591b51cd322036f2eda7
Shubham Sharma <sh1nine96@gmail.com>
2022-10-15 07:13:25 +0530

    Utility components - Loading indicator

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -4,13 +4,18 @@ import Home from "./Home";
 import { Container } from "react-bootstrap";
 import About from "./About";
 import imageUrls from "../data/image-sources";
+import LoadingIndicator from "./common/LoadingIndicator";
 const App = () => {
   return (
     <>
       <NavigationMenu />
       <Container>
         {/* <Home /> */}
-        <About imageUrls={imageUrls} />
+        {/* <About imageUrls={imageUrls} /> */}
+        <LoadingIndicator
+          size="medium"
+          message="Restaurants are being fetched"
+        />
       </Container>
     </>
   );
