c1964cbcea902712b6a03a78bd724b71dcc9c8bf
Shubham Sharma <sh1nine96@gmail.com>
2022-10-15 08:20:56 +0530

    rating-component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -5,6 +5,7 @@ import { Container } from "react-bootstrap";
 import About from "./About";
 import imageUrls from "../data/image-sources";
 import LoadingIndicator from "./common/LoadingIndicator";
+import Rating from "./common/Rating";
 const App = () => {
   return (
     <>
@@ -12,10 +13,12 @@ const App = () => {
       <Container>
         {/* <Home /> */}
         {/* <About imageUrls={imageUrls} /> */}
-        <LoadingIndicator
+        {/* <LoadingIndicator
           size="medium"
-          message="Restaurants are being fetched"
-        />
+          message="Restaurants are being fetched. Please wait...."
+        /> */}
+        <Rating value={3.75} className="me-2" />
+        3.75 (120 ratings)
       </Container>
     </>
   );
